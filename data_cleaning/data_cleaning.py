56230e6bc1fb8e39c781f9d2e3e1e3e9756f51fd
steamyrain <rainingsteam@gmail.com>
2019-08-24 15:50:21 +0700

    add propname entities

--- data_cleaning/data_cleaning.py
+++ data_cleaning/data_cleaning.py
@@ -1,5 +1,8 @@
 import langid
 import spacy
+from spacy.matcher import PhraseMatcher 
+from spacy.lang.en import English
+from spacy.tokens import Span
 import en_core_web_sm ## spacy's english model 
 from translate import Translator
 import pandas as pd
@@ -30,14 +33,14 @@ def source(texts,targets):
 
 ### load the installed model "en_core_web_sm"
 
-nlp = spacy.load('en_core_web_sm')
+# nlp = spacy.load('en_core_web_sm')
 
-docs = list(nlp.pipe(texts))
+# docs = list(nlp.pipe(texts))
 
 # for token in doc:
 #         print(type(token),token.lemma_.strip())
 
-print('without punctuations: ',[iter(doc) for doc in docs])
+# print('without punctuations: ',[iter(doc) for doc in docs])
 
 # print('without punctuations: ',[doc.Token.lemma_ for doc in docs if not doc.Token.is_punct])
 
@@ -83,5 +86,42 @@ print('without punctuations: ',[iter(doc) for doc in docs])
 
 # x = my_translate([])
 # print(x.send(texts[0]))
-df = pd.read_csv('dataset/mypersonality_final.csv',delimiter=',',encoding='latin')
-print(df.head())
+
+# Load dataset with pandas 
+df = pd.read_csv('dataset/mypersonality_final.csv',delimiter=',',encoding='latin',header=0)
+personality = df.iloc[0:,list(range(7,12))]
+ext = personality['cEXT'].apply(lambda x:x.replace('y','1') if x=='y' else x.replace('n','0'))
+neu = personality['cNEU'].apply(lambda x:x.replace('y','1') if x=='y' else x.replace('n','0'))
+agr = personality['cAGR'].apply(lambda x:x.replace('y','1') if x=='y' else x.replace('n','0'))
+con = personality['cCON'].apply(lambda x:x.replace('y','1') if x=='y' else x.replace('n','0'))
+opn = personality['cOPN'].apply(lambda x:x.replace('y','1') if x=='y' else x.replace('n','0'))
+personality = pd.concat([ext,neu,agr,con,opn],ignore_index=True,axis=1) ### gotta concat 'em all !, axis 1 == treating copy vertically(column wise), 
+                                                                        ### ignore index == ignore index from original source
+columnNames=['bEXT','bNEU','bAGR','bCON','bOPN']
+personality.columns = columnNames
+personality[columnNames] = personality[columnNames].astype(int)
+df = pd.concat([df,personality],axis=1)
+
+DATASET = df['STATUS']
+
+#Instantiate spacy model 
+# nlp = English()
+nlp = spacy.load('en_core_web_sm')
+
+#matcher using shared vocab 
+matcher = PhraseMatcher(nlp.vocab)
+matcher.add("PROPER_NAME",None,nlp.make_doc("*PROPNAME*"))
+
+def propname_component(doc):
+        matches = matcher(doc)
+        spans = [Span(doc,start,end,label="PROPER_NAME")for match_id,start,end in matches]
+        doc.ents = list(doc.ents) + spans
+        return doc
+
+nlp.add_pipe(propname_component,after='ner')
+print(nlp.pipe_names)
+buff = []
+for doc in nlp.pipe(DATASET):
+        buff += {doc for ent in doc.ents if ent.label_=="PERSON" or ent.label_=="PROPER_NAME"}
+
+print (buff)
