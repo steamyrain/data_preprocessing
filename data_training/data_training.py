13d863ed3edb6c76a8679be41e99c843adec855f
xiaoren <reinz.rawk@gmail.com>
2019-08-29 15:55:06 +0700

    add data_training.py

--- data_training/data_training.py
+++ data_training/data_training.py
@@ -1,1 +1,9 @@
 import pickle
+import spacy
+import en_core_web_sm
+import pandas as pd
+
+with open("dataset/doc-with-authid.p",'rb') as f:
+    docs = pickle.load(f)
+
+print([token.has_vector for token in docs[1]])
